128071c8a4f08724716c6c525b77c0d2d486b9d3
Artyom Efimov <artyomefimov97@gmail.com>
2019-09-17 17:01:38 +0400

    main controller #3

--- src/main/java/com/artyomefimov/web/controller/StartController.java
+++ src/main/java/com/artyomefimov/web/controller/StartController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class StartController {
     @GetMapping(value = "/")
     public String start() {
         return WebConstants.LOGIN_PAGE;
