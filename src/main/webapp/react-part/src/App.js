b411f6200930a6706e7a550eed71e352e3ab8320
Artyom Efimov <artyomefimov97@gmail.com>
2019-09-22 15:37:54 +0400

    удалены тестовые jsp, добавлен шаблон реакт приложения #5

---
+++ src/main/webapp/react-part/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import AutorepairApp from "./component/AutorepairApp";
+
+function App() {
+  return (
+    <div className="container">
+      <AutorepairApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/webapp/react-part/src/component/AutorepairApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AutorepairApp extends Component {
+    render() {
+        return (
+            <h1>Autorepair</h1>
+        )
+    }
+}
+
+export default AutorepairApp
